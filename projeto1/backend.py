357169e86bd0786e06d3283b72b62df65f72f519
Vinicius Pereira <vinippereira23@hotmail.com>
2015-09-29 23:12:47 -0300

    backend atualizado

--- projeto1/backend.py
+++ projeto1/backend.py
@@ -3,23 +3,20 @@ import socket
 def servidor(cmd):
     #print "entrou back"
     data = ''
-    host = ''
+    host = '127.0.0.1'
     porta = 5000
-    endereco = ((host, porta))
     conexaoServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conexaoServidor.bind(endereco)
-    # #escuta no maximo tres maquinas
-    conexaoServidor.listen(3)
-    # #aceita conexao
-    con, addrCli = conexaoServidor.accept()
+    destino = ((host, porta))
+    output = ""
+    conexaoServidor.connect(destino)
     #while True:
         #Envia comandos
     if cmd:
     #for comando in cmd:
-        con.send(cmd)
+        conexaoServidor.send(cmd)
         cmd=""
         #recebe e printa comandos
-        data += con.recv(1024)
+        data = conexaoServidor.recv(1024)
 
     conexaoServidor.close()
     return data
