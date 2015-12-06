fa70de70635a87dab3ea1777a056cf39264d6932
crispendenza <crispendenza@gmail.com>
2015-12-06 14:42:12 -0200

    arrumando erros e exceções

--- projeto2/2emissor.py
+++ projeto2/2emissor.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 #Servidor, responde requisicoes
 
 import socket
@@ -8,6 +9,9 @@ from dPacote import Pacote
 TIMEOUT = 1.1
 
 def Servidor(args):
+    if len(args) < 3:
+        print 'Entrada errada. execução se da por:\n\tpython emissor.py <porta> <nome_arquivo>'
+        sys.exit()
     #recebe a porta pela linha de comando
     port = int(args[1])
     #Nao consegui fazer o cliente pedir o arquivo, tem que entrar com o nome
@@ -18,21 +22,28 @@ def Servidor(args):
     pacoteRecebido = Pacote()
     janela = 3
     texto = LeArquivo(nomeArquivo)
+    if texto == False:
+       print 'Arquivo não existe ou não tem permissão para leitura:\n\tVerifique se o arquivo existe\n\tTente mudar a permissao (chmod)'
+       sys.exit()
     pacotes = CriaPacotes(texto)
-
+    #Descobre o endereço do cliente
+    print 'Esperando o cliente conectar'
+    msg, addr = RecebePacote(s)
+    print 'descobri o endereco: ', addr
     nackAntigo = 0
     nack = 0
     #Logica do rdt send
     while nackAntigo < len(pacotes):
         # Can we send a packet, do we need to send pkt
         if nack < janela and (nack + nackAntigo) < len(pacotes):
-            send_pkt(pacotes[nackAntigo + nack], s)
+            EnviaPacote(pacotes[nackAntigo + nack], s, addr)
+            print 'Enviando pacote'
             nack += 1
         else:
             #Espera pelos acks do cliente
             ready = select.select([s], [], [], TIMEOUT)
             if ready[0]:
-                pacoteRecebido, addr = RecebePacote()
+                pacoteRecebido, addr = RecebePacote(s)
             else: # Janela encheu
                 print "Atingiu timeout"
                 nack = 0
@@ -46,8 +57,9 @@ def Servidor(args):
     sock.close()
     sys.exit(0)
 
-def RecebePacote():
+def RecebePacote(s):
     ''' Recebe o pacote e coloca em um formato mais facil de trabalhar'''
+    pacoteRecebido = Pacote()
     msg, addr = s.recvfrom(4096)
     pacoteRecebido.ToPacote(msg)
     return pacoteRecebido, addr
@@ -58,14 +70,17 @@ def VerificaPacote(pacoteRecebido, host):
         return True
     return False
 
-def EnviaPacote(pacoteEnviar, s, addr, port):
-    ''' coloca o pacote no formato certo e envia'''
-    s.sendto(pacoteEnviar.ToString(), (addr, port))
+def EnviaPacote(pacoteEnviar, s, addr):
+    ''' coloca o pacote no formato correto e envia'''
+    s.sendto(pacoteEnviar.ToString(), addr)
 
 def LeArquivo(nome):
     ''' Le o arquivo e joga tudo em uma variavel '''
-    texto = open(nome, 'r')
-    return texto.read()
+    try:
+        texto = open(nome, 'r')
+        return texto.read()
+    except Exception:
+        return False
 
 def CriaPacotes(texto):
     ''' forma uma lista de pacotes que contem todos os pacotes que serao enviados '''
@@ -77,13 +92,13 @@ def CriaPacotes(texto):
         pacote.numeroSequencia = numeroSequencia
         pacote.ack = 0
         #Envia de 20 em 20 caracteres
-        pacote.data = text[:20]
-        pacote.checksum = pacote.CalculaChecksum(pacote.data)
+        pacote.data = texto[:20]
+        pacote.checksum = pacote.CalculaChecksum(texto[:20])
         pacote.chegou = False
         #adciona o pacote na lista de pacotes
         pacotes.append(pacote)
-        texto = texto[:20]
-        seq_num += 1
+        texto = texto[20:]
+        numeroSequencia += 1
     # Newly built list of pacotes
     return pacotes
 
